12d0b0abaf9037832c39998adac06cce48edd135
Sadia Qazi <test@test.se>
2022-10-04 16:24:02 +0200

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.println("Felix är häst");
+        System.out.println("TACKAR");
     }
-
 }
