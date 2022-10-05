a912967d6bed5a31c7f21a8531968d0bd87ef67f
felix <Felix.Elfgren@awacademy.training>
2022-10-05 11:17:54 +0200

    Added main whileloop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        playGame();
 
     }
-
     static void playGame() throws IOException {
 
         Terminal terminal = terminalBuilder();
@@ -15,15 +17,36 @@ public class Main {
 
     static Terminal terminalBuilder() {
 
+        boolean isDead = false;
+        int score = 0;
+
+
         try {
+            TerminalSize ts = new TerminalSize (60, 50);
+                                                                            //       TerminalPosition tp = new TerminalPosition();
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+            terminalFactory.setInitialTerminalSize(ts);
             Terminal terminal = terminalFactory.createTerminal();
             return terminal;
+
         } catch (IOException e) {
             System.out.println(e.getStackTrace());
             return null;
-        }
 
+
+        while(!isDead) {
+            String scoreString = String.format("Score: %04d", score);
+
+            for(int i = 0; i < scoreString.length(); ++i) {
+                terminal.setCursorPosition(49 + i, 0);
+                terminal.putCharacter(scoreString.charAt(i));
+
+                }
     }
+
+
+        }
+    }
+
 
 }
