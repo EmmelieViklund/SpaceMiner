7beec171794bc8c99025407bc8a208637db8b23c
Emmelie <emmelie.wiklund@gmail.com>
2022-10-05 11:18:37 +0200

    MovePlayer method created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import Posistion.Position;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
@@ -6,6 +7,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
+        Spaceship spaceship = new Spaceship(new Position(20, 50));
+        spaceship.playerChar
     }
 
     static void playGame() throws IOException {
--- src/main/java/Spaceship.java
+++ src/main/java/Spaceship.java
@@ -1,3 +1,37 @@
+import Posistion.Position;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.security.Key;
+
 public class Spaceship {
+    final char playerChar = '\u0020';
+
+    Position playerPosition;
+
+    public Spaceship(Position playerPosition) {
+        this.playerPosition = playerPosition;
+    }
+
+    public Position movePlayer (KeyType direction) {
+
+   switch (direction) {
+
+       case ArrowLeft-> {
+            this.playerPosition.setX(playerPosition.getX() - 1);
+            return this.playerPosition;
+        }
+
+        case ArrowRight -> {
+            this.playerPosition.setX(playerPosition.getX() + 1);
+            return this.playerPosition;
+        }
+
+    }
+}
+
+
 }
 
