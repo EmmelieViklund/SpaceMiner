c2f4c07b54ad97a5aa7dd27399493916a66d3305
felix <Felix.Elfgren@awacademy.training>
2022-10-05 09:53:33 +0200

    Added factory and terminal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
-package org.example;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Felix är häst");
+    }
+
+    static void playGame() throws IOException {
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Terminal terminal = terminalFactory.createTerminal();
+
+
     }
 
 }
