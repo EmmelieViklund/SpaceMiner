ab34501b6eabdf9e10d6eb5753dbf0713e7aa63a
Emmelie <emmelie.wiklund@gmail.com>
2022-10-07 08:41:28 +0200

    Update spacehip

--- src/main/java/Asteroid.java
+++ src/main/java/Asteroid.java
@@ -1,32 +1,56 @@
 import Posistion.Position;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.terminal.Terminal;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Asteroid {
 
-    ArrayList<Asteroid> asteroids;
-    final char block= 9608;
+    final char block = 9608;
     int size;
 
+    Position asteroidPosition;
 
-    public Asteroid(Position position) {
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Asteroid() {
         Random random = new Random();
+        this.asteroidPosition = new Position(random.nextInt(0, 50), 0);
+
         int i = random.nextInt(0, 10);
-        switch(i){
-            case 0,1,2-> {
-              size=2;
+        switch (i) {
+            case 0, 1, 2 -> {
+                size = 2;
             }
-            case 3,4,5->{
-                size=3;
+            case 3, 4, 5 -> {
+                size = 3;
             }
-            case 6,7->{
-                size=1;
+            case 6, 7 -> {
+                size = 1;
             }
-            case 8,9->{
-                size=5;
+            case 8, 9 -> {
+                size = 5;
             }
         }
+
+    }
+
+    public void moveAsteroid(Terminal terminal, KeyStroke keyStroke) throws Exception {
+        int oldX = asteroidPosition.getX(), oldY = asteroidPosition.getY();
+
+
+        terminal.setCursorPosition(asteroidPosition.getX(), asteroidPosition.getY());
+        terminal.putCharacter(block);
+        terminal.setCursorPosition(oldX, oldY);
+        terminal.putCharacter(' ');
+        terminal.flush();
 
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,11 +21,18 @@ public class Main {
         KeyStroke keyStroke;
 
         Spaceship spaceship = new Spaceship(new Position(30, 37));
-        Asteroid asteroid = new Asteroid(new Position(15,10));
-        terminal.setCursorPosition(asteroid.playerPosition.getX, asteroid.playerPosstion.getY());
+        List<Asteroid> asteroids = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Asteroid asteroid = new Asteroid();
+            asteroids.add(asteroid);
+            terminal.setCursorPosition(asteroid.asteroidPosition.getX(), asteroid.asteroidPosition.getY());
+            terminal.putCharacter((asteroid.block));
+        }
+
         terminal.setCursorPosition(spaceship.playerPosition.getX(), spaceship.playerPosition.getY());
         terminal.putCharacter(spaceship.playerChar);
         terminal.flush();
+
 
         while (!isDead) {
             do {
@@ -34,7 +41,7 @@ public class Main {
 
             } while (keyStroke == null);
 
-            List<Asteroid> asteroids = new ArrayList<Asteroid>();
+
             spaceship.movePlayer(terminal, keyStroke);
             spaceship.fire(terminal, keyStroke, asteroids);
             //moveAstroids
